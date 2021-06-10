042cb1f8d5920920da0fec26bbe417dbb2a2f33a
Damir92 <home_damir92@mail.ru>
2021-06-10 14:18:13 +0300

    Add configuration

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b534a955f17629eda4b163727359c21'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5d74deca-4471-45c0-829c-01f38f996644'
+};
